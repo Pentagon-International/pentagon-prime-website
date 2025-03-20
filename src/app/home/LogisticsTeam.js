334eab1fb8579f4848bac8cb595b135c1c3ed3e1
santhosh <santhosh.p@pentafox.in>
2025-03-20 13:42:43 +0530

    feat(LogisticTeam): make page as mobile responsive

--- src/app/home/LogisticsTeam.js
+++ src/app/home/LogisticsTeam.js
@@ -1,3 +1,4 @@
+'use client';
 import { client } from '@/app/api/contentful';
 import { COLORS } from '@/app/utils/COLORS';
 import { highlightText } from '@/app/utils/highlightText';
@@ -11,108 +12,94 @@ import {
   Title,
   Box,
 } from '@mantine/core';
-import React from 'react';
+import { useMediaQuery } from '@mantine/hooks';
+import React, { useEffect, useState } from 'react';
 
 const TeamMember = ({ image, title, description, reverse }) => {
   const imageUrl = image ? `https:${image}` : '/placeholder.jpg';
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   return (
-    <Grid py={50} align="center" w={'100%'}>
-      {!reverse && (
-        <GridCol px={'xl'} span={6}>
-          <Box pos="relative" w="90%" h="280px">
-            <Box
-              w={'100%'}
-              h={'100%'}
-              style={{
-                position: 'absolute',
-                backgroundColor: COLORS.globalCardBackground,
-                borderRadius: '12px',
-                zIndex: 0,
-              }}
-            />
-            <Image
-              src={imageUrl}
-              alt={title || 'Team Member'}
-              style={{
-                width: '100%',
-                height: '100%',
-                borderRadius: '12px',
-                objectFit: 'cover',
-                position: 'relative',
-                top : '30px',
-                left : '36px',
-                zIndex: 1,
-              }}
-            />
-          </Box>
-        </GridCol>
-      )}
-      <GridCol px={'xl'} span={6}>
-        <Title tt="uppercase" fw={800} lh="lgx2" size="lg" style={{ color: 'black' }}>
+    <Grid py={ isMobile ? 30 : 50} align="center" w={'100%'} gutter="xl">
+      <GridCol px={ !isMobile && 'xl'} span={isMobile ? 12 : 6} order={!isMobile && reverse ? 2 : 1}>
+        <Box pos="relative" w={isMobile ? "100%" : "90%"} h="280px">
+          <Box
+            w={'100%'}
+            h={'100%'}
+            style={{
+              position: 'absolute',
+              backgroundColor: COLORS.globalCardBackground,
+              borderRadius: '12px',
+              zIndex: 0,
+            }}
+          />
+          <Image
+            src={imageUrl}
+            alt={title || 'Team Member'}
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '12px',
+              objectFit: 'cover',
+              position: 'absolute',
+              zIndex: 1,
+            }}
+          />
+        </Box>
+      </GridCol>
+      <GridCol px={ !isMobile && 'xl'} span={isMobile ? 12 : 6} order={reverse ? 1 : 2}>
+        <Title tt="uppercase" fw={800} lh={isMobile ? "md" : "lgx2"} size={isMobile ? "md" : "lg"} style={{ color: 'black' }}>
           {highlightText(title)}
         </Title>
-        <Text mt={10} c={COLORS.textColor} maw={'80%'}  size="base" lh="sm">
+        <Text mt={10} c={COLORS.textColor} maw={  isMobile ? '100%' : '80%'} size="base" lh="sm">
           {highlightText(description)}
         </Text>
       </GridCol>
-      {reverse && (
-        <GridCol px={'xl'} span={6}>
-          <Box pos="relative" w="90%" h="280px">
-            <Box
-              w={'100%'}
-              h={'100%'}
-              style={{
-                position: 'absolute',
-                backgroundColor: COLORS.globalCardBackground,
-                borderRadius: '12px',
-                zIndex: 0,
-              }}
-            />
-            <Image
-              src={imageUrl}
-              alt={title || 'Team Member'}
-              style={{
-                width: '100%',
-                height: '100%',
-                borderRadius: '12px',
-                objectFit: 'cover',
-                position: 'relative',
-                top : '31px',
-                left : '36px',
-                zIndex: 1,
-              }}
-            />
-          </Box>
-        </GridCol>
-      )}
     </Grid>
   );
 };
 
-const LogisticsTeam = async ({ title, content }) => {
-  const res = await client.getEntries({
-    content_type: 'logisticsTeam',
-    order: 'sys.createdAt',
-  });
+const LogisticsTeam = ({ title, content }) => {
+  const [teamData, setTeamData] = useState([]);
+
+  const isMobile = useMediaQuery('(max-width: 768px)');
+
+  useEffect(() => {
+    const fetchTeamData = async () => {
+      try {
+        const res = await client.getEntries({
+          content_type: 'logisticsTeam',
+          order: 'sys.createdAt',
+        });
+        setTeamData(res.items || []);
+      } catch (error) {
+        console.error('Error fetching team data:', error);
+      }
+    };
+
+    fetchTeamData();
+  }, []);
 
   return (
-    <Container fluid px={'7%'} py={'70px'}>
-      <Title  size={'lg'} lh={'lgx2'} tt={'uppercase'} fw={800}>
+    <Container fluid px={ '7%'} py={'70px'}>
+      <Title size={ isMobile ? '22px' : 'lg'} lh={  isMobile ? 'md' : 'lgx2'} tt={'uppercase'} fw={800}>
         {highlightText(title)}
       </Title>
-      <Text size="sm" maw={'55%'} fw={500} c={COLORS.textColor}>
+      <Text size={  isMobile ? "md" : "sm"} py={'sm'} maw={ isMobile ? '80%' : '55%'} fw={500} c={COLORS.textColor}>
         {highlightText(content)}
       </Text>
-      <Flex wrap="wrap" align="center" mt="30px" justify="center" gap="md">
-        {res.items?.map((item, index) => {
-          const imageUrl = item.fields.logisticsTeamImage?.fields?.file?.url || item.fields.imageUrl || null;
+      <Flex  wrap="wrap" align="center" mt="30px" justify="center" gap="md">
+        {teamData?.map((item, index) => {
+          const fields = item?.fields || {};
+          const imageUrl =
+            fields.logisticsTeamImage?.fields?.file?.url || fields.imageUrl || null;
+
           return (
             <TeamMember
               key={index}
               image={imageUrl}
-              title={item.fields.teamTitle}
-              description={item.fields.teamDescription}
+              title={fields.teamTitle}
+              description={fields.teamDescription}
               reverse={index % 2 !== 0}
             />
           );
