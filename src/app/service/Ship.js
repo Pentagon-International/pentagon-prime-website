be6952bff50133bf11647b252ebbc0aaf704b684
santhosh <santhosh.p@pentafox.in>
2025-04-01 18:31:35 +0530

    feat(solutions) - add a dyanamic page for shipAnyWhere and shipEveryWhere

--- src/app/service/Ship.js
+++ src/app/service/Ship.js
@@ -9,31 +9,14 @@ import {
   Text,
   Title,
 } from '@mantine/core';
-import { client } from '../api/contentful';
 import { COLORS } from '../utils/COLORS';
 import { highlightText } from '../utils/highlightText';
 import { useMediaQuery } from '@mantine/hooks';
-import { useEffect, useState } from 'react';
 import { Carousel } from '@mantine/carousel';
 
-const Ship = ({ first_title, first_content, second_title, second_content }) => {
-  const [serviceData, setServiceData] = useState([]);
+const Ship = ({ first_title, first_content, serviceData }) => {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
-  useEffect(() => {
-    const fetchServiceData = async () => {
-      try {
-        const res = await client.getEntries({
-          content_type: 'shipment',
-          order: 'sys.createdAt',
-        });
-        setServiceData(res.items || []);
-      } catch (error) {
-        console.error("Error fetching service data:", error);
-      }
-    };
-    fetchServiceData();
-  }, []);
 
   return (
     <Container fluid px={'7%'} py={isMobile ? 0 : '10px'} mb={isMobile ? 20 : 160}>
@@ -46,86 +29,42 @@ const Ship = ({ first_title, first_content, second_title, second_content }) => {
           {isMobile ? (
             <Grid w={'100%'}>
               <Carousel slideSize="80%" w={'100%'} height={450} align={'start'} slideGap="md" loop withControls={false}>
-              {serviceData.map((item) => (
-                <Carousel.Slide key={item.sys.id} w={'100%'}>
-                <GridCol key={item.sys.id} span={15} mt={'md'}>
-                  <Image
-                    src={item.fields.image?.fields?.file?.url}
-                    alt={item.name}
-                  />
-                  <Text size="sm" fw={500} mt={20}>
-                    {item.fields.title}
-                  </Text>
-                  <Text size='smx' mt={10} color="dimmed" lh={isMobile ? '20px' : ''}>
-                    {item.fields.description}
-                  </Text>
-                </GridCol>
-                </Carousel.Slide>
-              ))}
-            </Carousel>
-            </Grid>
-          ) : (
-            <Grid columns={3} gutter={90} mt={60}>
-              {serviceData.map((item) => (
-                <GridCol key={item.sys.id} span={1}>
-                  <Image
-                    src={item.fields.image?.fields?.file?.url}
-                    alt={item.name}
-                  />
-                  <Text size="sm" fw={500} mt={20}>
-                    {item.fields.title}
-                  </Text>
-                  <Text size='smx' mt={10} color="dimmed">
-                    {item.fields.description}
-                  </Text>
-                </GridCol>
-              ))}
-            </Grid>
-          )}
-        </Flex>
-
-        <Flex direction={'column'}>
-          <Title size={'lg'} tt={'uppercase'}>{highlightText(second_title)}</Title>
-          <Text size='sm' c={COLORS.textColor} w={isMobile ? '100%' : '40vw'} lh={isMobile ? '20px' : ''}>
-            {highlightText(second_content)}
-          </Text>
-          {isMobile ? (
-            <Grid>
-              <Carousel slideSize="80%" w={'100%'} height={450} align={'start'} slideGap="md" loop withControls={false}>
-                {serviceData.map((item) => (
+              {serviceData
+                ?.sort((a, b) => (a.fields.order || 0) - (b.fields.order || 0)).map((item) => (
                   <Carousel.Slide key={item.sys.id} w={'100%'}>
-                  <GridCol key={item.sys.id} span={15} mt={'md'}>
-                    <Image
-                      src={item.fields.image?.fields?.file?.url}
-                      alt={item.name}
-                    />
-                    <Text size="sm" fw={500} mt={20}>
-                      {item.fields.title}
-                    </Text>
-                    <Text mt={10} size='smx' color="dimmed" lh={isMobile ? '20px' : ''}>
-                      {item.fields.description}
-                    </Text>
-                  </GridCol>
+                    <GridCol key={item.sys.id} span={15} mt={'md'}>
+                      <Image
+                        src={item.fields.image?.fields?.file?.url}
+                        alt={item.name}
+                      />
+                      <Text size="sm" fw={500} mt={20}>
+                        {item.fields.title}
+                      </Text>
+                      <Text size='smx' mt={10} color="dimmed" lh={isMobile ? '20px' : ''}>
+                        {item.fields.description}
+                      </Text>
+                    </GridCol>
                   </Carousel.Slide>
                 ))}
               </Carousel>
             </Grid>
           ) : (
-            <Grid columns={3} gutter={90} mt={60}>
-              {serviceData.map((item) => (
-                <GridCol key={item.sys.id} span={1}>
-                  <Image
-                    src={item.fields.image?.fields?.file?.url}
-                    alt={item.name}
-                  />
-                  <Text size="sm" fw={500} mt={20}>
-                    {item.fields.title}
-                  </Text>
-                  <Text mt={10} size='smx' color="dimmed">
-                    {item.fields.description}
-                  </Text>
-                </GridCol>
-              ))}
+            <Grid columns={3} gutter={70} mt={60}>
+              {serviceData
+                ?.sort((a, b) => (a.fields.order || 0) - (b.fields.order || 0)).map((item) => (
+                  <GridCol key={item.sys.id} span={1}>
+                    <Image
+                      src={item.fields.image?.fields?.file?.url}
+                      alt={item.name}
+                    />
+                    <Text size="md" fw={500} mt={20}>
+                      {item.fields.title}
+                    </Text>
+                    <Text size='sm' mt={10} color="dimmed">
+                      {item.fields.description}
+                    </Text>
+                  </GridCol>
+                ))}
             </Grid>
           )}
         </Flex>
--- src/app/utils/fetchEntries.js
+++ src/app/utils/fetchEntries.js
@@ -9,3 +9,19 @@ export const fetchEntries = async (reference) => {
 
   return res.items[0]?.fields || {};
 };
+
+
+export const fetchShippmententries = async (reference) => {
+  try {
+    const res = await client.getEntries({
+      content_type: 'shipment',
+      "fields.reference": reference, 
+      order: 'sys.createdAt',
+    });
+
+    return res.items || [];
+  } catch (error) {
+    console.error("Error fetching shipment entries:", error);
+    return [];
+  }
+};
